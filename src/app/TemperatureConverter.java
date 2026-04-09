13f4c7254dec8aeb761d0f3daf42b2b9e8cc0e6c
SunnyTany <46300377+SunnyTany@users.noreply.github.com>
2026-04-10 00:32:25 +0200

    add converter methods

--- src/app/TemperatureConverter.java
+++ src/app/TemperatureConverter.java
@@ -1,7 +1,20 @@
 package app;
 
 public class TemperatureConverter {
-    public static void main(String[] args) {
 
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+
+    public static void main(String[] args) {
+        double tempC = 25.0;
+        double tempF = 77.0;
+
+        System.out.println(tempC + "°C = " + celsiusToFahrenheit(tempC) + "°F");
+        System.out.println(tempF + "°F = " + fahrenheitToCelsius(tempF) + "°C");
     }
 }
